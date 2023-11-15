80f924eb8f501a1099eece92df9522f74a3d889d
Rik de Goede <rik.dgd@gmail.com>
2023-11-15 17:15:42 +0100

    Add Messenger as singleton
    The application can now use a messenger instance across all controllers with its own lifetime.

--- Controllers/WorkspaceController.cs
+++ Controllers/WorkspaceController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using CodeCollab___Gateway.Models;
+using CodeCollab___Gateway.Utils;
+
 
 namespace CodeCollab___Gateway.Controllers;
 
@@ -6,6 +10,14 @@ namespace CodeCollab___Gateway.Controllers;
 [Route("[controller]")]
 public class WorkspaceController : ControllerBase
 {
+    private readonly Messenger _messenger;
+
+    public WorkspaceController(Messenger messenger)
+    {
+        _messenger = messenger;
+    }
+    
+    
     [HttpGet(Name = "GetWorkspace")]
     public async Task<IActionResult> GetWorkspace(long id)
     {
@@ -13,6 +25,35 @@ public class WorkspaceController : ControllerBase
         string content = await GetById(url, id);
         return Ok(content);
     }
+
+
+    [HttpPost(Name = "CreateWorkspace")]
+    public IActionResult CreateWorkspace([FromBody] WorkspaceModel workspace)
+    {
+        try
+        {
+            string workspaceJson = JsonSerializer.Serialize(workspace);
+
+            // using (Messenger messenger = new Messenger(
+            //            hostName: "localhost",
+            //            appName: "Gateway",
+            //            exchangeName: "workspace-exchange",
+            //            queueName: "workspace",
+            //            isConsumer: false))
+            // {
+            //     messenger.SendMessage($"CREATE Workspace FROM: {workspaceJson}");
+            // }
+            _messenger.SendMessage($"CREATE Workspace FROM: {workspaceJson}");
+        
+            return Ok($"Successfully created workspace from JSON:\n {workspaceJson}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest("Failed to create the new workspace.");
+        }
+    }
+    
 
     // ToDo: Error handling
     private async Task<string> GetById(string url,  long id)
